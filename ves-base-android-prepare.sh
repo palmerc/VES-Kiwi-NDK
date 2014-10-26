7e6dcd2be7b06fab2eb18495e31c8e339de4a794
Cameron Lowell Palmer <cameron.palmer@gmail.com>
2014-10-26 15:12:57 +0100

    Initial commit

---
+++ ves-base-android-prepare.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+. ./ves-kiwi-variables.sh
+
+### Build VES/VTK
+if [ ! -d ${VES_SOURCE_DIR} ]; then
+   mkdir -p ${VES_SOURCE_DIR} ${VES_BUILD_DIR}
+   git clone ${VES_OFFICIAL_REPO} ${VES_SOURCE_DIR} 
+else
+   echo "VES source directory exists at ${VES_SOURCE_DIR}."
+fi
