7e9c31ae7b0b86cfadd9ffbe663681c10340e0c8
Cameron Lowell Palmer <cameron.palmer@gmail.com>
2014-10-26 15:46:12 +0100

    Add build script for libvtk

---
+++ libvtk-android-create.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+. ./ves-kiwi-variables.sh
+
+LIBVTK_DIR=${SCRIPT_DIR}/libvtk-android
+LIBVTK_FILE=libvtk-android.a
+
+VTK_LIB_DIR=${VES_BUILD_DIR}/CMakeExternals/Install/vtk-android/lib
+VTK_INCLUDE_DIR=${VES_BUILD_DIR}/CMakeExternals/Install/vtk-android/include/vtk-6.0
+
+mkdir -p "${LIBVTK_DIR}"/{include,lib}
+cd ${VTK_LIB_DIR}
+"${GNU_AR_EXECUTABLE}" -M < ${SCRIPT_DIR}/libvtk-android-script
+mv "${VTK_LIB_DIR}/${LIBVTK_FILE}" "${LIBVTK_DIR}/lib"
+cp -R "${VTK_INCLUDE_DIR}/" "${LIBVTK_DIR}/include"
