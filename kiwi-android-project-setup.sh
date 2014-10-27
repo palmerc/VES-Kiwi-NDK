ad293898278c861ade31e1ae7678ea6c202ead4f
Cameron Lowell Palmer <cameron.palmer@gmail.com>
2014-10-27 11:11:46 +0100

    Added the shaders into the app

--- kiwi-android-project-setup.sh
+++ kiwi-android-project-setup.sh
@@ -18,10 +18,14 @@ cp -R "${LIBVTK_DIR}" "${KIWI_JNI_DIR}/"
 
 # Copy ves, kiwi out of source
 cp -R "${VES_SOURCE_DIR}"/src/{kiwi,ves} "${KIWI_JNI_DIR}/ves-android/"
-rm -rf "${KIWI_JNI_DIR}/ves-android/ves/Testing"
-rm -rf "${KIWI_JNI_DIR}/ves-android/kiwi/Testing"
-rm "${KIWI_JNI_DIR}/ves-android/kiwi/vesKiwiTestHelper.h"
-rm "${KIWI_JNI_DIR}/ves-android/ves/vesTestHelper.h"
+cp "${VES_BUILD_BUILD_DIR}/ves-android/src/kiwi/vesKiwiOptions.h" "${KIWI_JNI_DIR}/ves-android/kiwi/"
+
+# Copy shaders out of the build directory.
+cp -R "${VES_BUILD_BUILD_DIR}/ves-android/src/shaders" "${KIWI_JNI_DIR}/ves-android/"
+#rm -rf "${KIWI_JNI_DIR}/ves-android/ves/Testing"
+#rm -rf "${KIWI_JNI_DIR}/ves-android/kiwi/Testing"
+#rm "${KIWI_JNI_DIR}/ves-android/kiwi/vesKiwiTestHelper.h"
+#rm "${KIWI_JNI_DIR}/ves-android/ves/vesTestHelper.h"
 
 # Clean out the cruft
 find ${KIWI_BUILD_DIR} -type f -name "*.cmake" -exec rm {} \;
